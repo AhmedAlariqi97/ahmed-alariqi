5d2392e5a463c26f5b410146166acc8e46dd2a21
unknown <ahmedalariqi97@gmail.com>
2024-02-26 00:04:16 +0300

    add projects html

--- src/components/4-main/myProjects.js
+++ src/components/4-main/myProjects.js
@@ -4,7 +4,7 @@ export const myProjects = [
   { title: "Growth ERP Company", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./growth.png" },
   { title: "Yemen E-market", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./yemenADs.png" },
   { title: "Gadah Jadaan", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./gadahJadaan.png" },
-  { title: "Gym", category: ["html"], descreption: "Built this User Interface as front end use Html, Css, JavaScript", imgPath: "./" },
+  { title: "Portfolio Website", category: ["html"], descreption: "Built this User Interface as front end use Html, Css, JavaScript", imgPath: "./portfolio-ui.png" },
   { title: "E-commerce", category: ["html"], descreption: "Built this User Interface as front end use Html, Css, JavaScript", imgPath: "./" },
   { title: "Topic", category: ["html"], descreption: "Built this User Interface as front end use Html, Css, JavaScript", imgPath: "./" },
   { title: "Wirx", category: ["react"], description: "Built this User Interface as front end use ReactJS", imagPath: "./"},

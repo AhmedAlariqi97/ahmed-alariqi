0bd38fb37610fb8c3723fc45bb0b4c97ea86ab9d
unknown <ahmedalariqi97@gmail.com>
2024-02-25 21:37:44 +0300

    edit my project filters

--- src/components/4-main/myProjects.js
+++ src/components/4-main/myProjects.js
@@ -1,10 +1,13 @@
 
 export const myProjects = [
-    {title: "Al-lord Group", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./al-lord.png" },
-    {title: "Growth ERP Company", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./growth.png" },
-    {title: "Yemen E-market", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./yemenADs.png" },
-    {title: "Wirex", category: ["html"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./gadahJadaan.png" },
-    {title: "Gym", category: ["react"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./gadahJadaan.png" },
-    {title: "E-commerce", category: ["node"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./gadahJadaan.png" },
-    {title: "Heeli", category: ["react"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./gadahJadaan.png" }
-  ]
+  { title: "Al-lord Group", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./al-lord.png" },
+  { title: "Growth ERP Company", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./growth.png" },
+  { title: "Yemen E-market", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./yemenADs.png" },
+  { title: "Gadah Jadaan", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./gadahJadaan.png" },
+  { title: "Gym", category: ["html"], descreption: "Built this User Interface as front end use Html, Css, JavaScript", imgPath: "./" },
+  { title: "E-commerce", category: ["html"], descreption: "Built this User Interface as front end use Html, Css, JavaScript", imgPath: "./" },
+  { title: "Topic", category: ["html"], descreption: "Built this User Interface as front end use Html, Css, JavaScript", imgPath: "./" },
+  { title: "Portfolio Website", category: ["react"], descreption: "Built this User Interface as front end use ReactJS", imgPath: "./" },
+  { title: "dashboard", category: ["node"], descreption: "Built this User Interface as front end use Node.js", imgPath: "./" },
+  { title: "Wirx", category: ["react"], description: "Built this User Interface as front end use ReactJS", imagPath: "./"}
+]

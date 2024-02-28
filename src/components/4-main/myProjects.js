d30183ff540f290470811bbb9c2419104ba70a25
unknown <ahmedalariqi97@gmail.com>
2024-02-29 00:04:05 +0300

    add image of project

--- src/components/4-main/myProjects.js
+++ src/components/4-main/myProjects.js
@@ -6,9 +6,9 @@ export const myProjects = [
   { title: "Gadah Jadaan", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./gadahJadaan.png" },
   { title: "Portfolio Website", category: ["html"], descreption: "Built this User Interface as front end use Html, Css, JavaScript", imgPath: "./portfolio-ui.png" },
   { title: "E-commerce", category: ["html"], descreption: "Built this User Interface as front end use Html, Css, JavaScript", imgPath: "./Ecommerce-UI.png" },
-  { title: "Topic", category: ["html"], descreption: "Built this User Interface as front end use Html, Css, JavaScript", imgPath: "./" },
-  { title: "Wirx", category: ["react"], description: "Built this User Interface as front end use ReactJS", imagPath: "./"},
-  { title: "Helpi", category: ["react"], description: "Built this User Interface as front end use ReactJS", imagPath: "./"},
+  { title: "Topic Clone", category: ["html"], descreption: "Built this User Interface as front end use Html, Css, JavaScript", imgPath: "./Topic-Clone.png" },
   { title: "dashboard", category: ["node"], descreption: "Built this User Interface as front end use Node.js", imgPath: "./" },
   { title: "Ministry Health", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./MinistryHealth.png" },
+  { title: "Wirxe Clone", category: ["react"], descreption: "Built this User Interface as front end use ReactJS Next js", imgPath: "./WirxeCloneNext-UI.png" },
+  { title: "Coffee Home", category: ["html"], descreption: "Built this User Interface as front end use Html, Css, JavaScript", imgPath: "./Coffee.png" },
 ]

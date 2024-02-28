33ab2f0627ecbbd2ca2acf773d83dfe1c658b6d3
unknown <ahmedalariqi97@gmail.com>
2024-02-28 21:59:23 +0300

    edit project data

--- src/components/4-main/myProjects.js
+++ src/components/4-main/myProjects.js
@@ -5,9 +5,10 @@ export const myProjects = [
   { title: "Yemen E-market", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./yemenADs.png" },
   { title: "Gadah Jadaan", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./gadahJadaan.png" },
   { title: "Portfolio Website", category: ["html"], descreption: "Built this User Interface as front end use Html, Css, JavaScript", imgPath: "./portfolio-ui.png" },
-  { title: "E-commerce", category: ["html"], descreption: "Built this User Interface as front end use Html, Css, JavaScript", imgPath: "./" },
+  { title: "E-commerce", category: ["html"], descreption: "Built this User Interface as front end use Html, Css, JavaScript", imgPath: "./Ecommerce-UI.png" },
   { title: "Topic", category: ["html"], descreption: "Built this User Interface as front end use Html, Css, JavaScript", imgPath: "./" },
   { title: "Wirx", category: ["react"], description: "Built this User Interface as front end use ReactJS", imagPath: "./"},
   { title: "Helpi", category: ["react"], description: "Built this User Interface as front end use ReactJS", imagPath: "./"},
-  { title: "dashboard", category: ["node"], descreption: "Built this User Interface as front end use Node.js", imgPath: "./" }
+  { title: "dashboard", category: ["node"], descreption: "Built this User Interface as front end use Node.js", imgPath: "./" },
+  { title: "Ministry Health", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./MinistryHealth.png" },
 ]

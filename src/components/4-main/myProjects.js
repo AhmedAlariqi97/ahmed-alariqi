a8793e626979f79332b0bd50bba8241c76c590d7
unknown <ahmedalariqi97@gmail.com>
2024-04-14 16:59:28 +0300

    add category

--- src/components/4-main/myProjects.js
+++ src/components/4-main/myProjects.js
@@ -1,11 +1,12 @@
 
 export const myProjects = [
-  { title: "Aura Store", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./AuraParma.png" },
-  { title: "Ahl Almadina Store", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./ahlmp.png" },
-  { title: "Smsm Store", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./Smsm.png" },
+  { title: "Aura Store", category: ["E-commerce"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./AuraParma.png" },
+  { title: "Ahl Almadina Store", category: ["E-commerce"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./ahlmp.png" },
+  { title: "Smsm Store", category: ["E-commerce"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./Smsm.png" },
   { title: "Al-lord Group", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./al-lord.png" },
   { title: "Growth ERP Company", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./growth.png" },
   { title: "U-Tern", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./Utern.png" },
+  { title: "Yemen House", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./Yemen-House.png" },
   { title: "ShopToday", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./ShopToday.png" },
   { title: "Bayanat Minist", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./bayanat.png" },
   { title: "Doktoori", category: ["html", "laravel"], descreption: "Built use Html, Css, JavaScript and Laravel 10 with MySql DB", imgPath: "./daktoori.png" },
